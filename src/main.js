cd3b3628566d076d6f98f18d7666d5a88fce99df
Antony <antonyc828@gmail.com>
2019-10-27 22:38:42 -0300

    added suport for routes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
+import VueCookies from 'vue-cookies';
 import App from './App.vue'
+
+import Router from './routes/index';
+
+Vue.use(VueRouter);
+Vue.use(VueCookies);
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router: Router,
 }).$mount('#app')
