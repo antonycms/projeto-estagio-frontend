fb36654161206fbae9d38ad8484e8fb308e301ea
Antony <antonyc828@gmail.com>
2019-10-29 00:46:05 -0300

    added token expiration check

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,21 @@
 import VueRouter from 'vue-router';
 import routes from './routes';
+import api from '../services/api'
 
 const Router = new VueRouter({ mode: 'history', routes });
 
-Router.beforeEach((to, from, next) => {
+Router.beforeEach(async (to, from, next) => {
+  try {
+    await api.get('/', {
+      headers: {
+        authorization: ('bearer ' + window.$cookies.get('token')),
+      }
+    });
+  } catch (err) {
+    window.$cookies.remove('token');
+    window.$cookies.remove('user');
+  }
+
   if (to.meta.requiresAuth) {
     if (!window.$cookies.get('token')) {
       next({ path: '/' });
